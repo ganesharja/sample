ac1d0ec8d4e57df6a0ffc6e1bc606e3834b76307
Ganesh Arja <jayaganesharja.com>
2024-04-11 15:06:04 +0530

    Auto push

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -1,10 +1,19 @@
+# Datasource to fetch the info of AMI
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "b55_ansible_image_WS"
+  owners           = ["self"]
+}
+
+
 resource "aws_instance" "web" {
   # ami                       = "ami-0e977b99c38a13dfc"
   count                     =var.howManyYouWant
-  ami                       = "ami-0081f769161bc01f5"
+  ami                       = aws_ami.ami.image_id
   instance_type             = "t3.medium"
 
   tags = {
+ 
     Name = "Terraform-web-Instance-${count.index+1}"
   }
 }
